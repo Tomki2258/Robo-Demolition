42b1065ee8929439af01babc3230e79fdf90559e
tamus <52979692+Tomki2258@users.noreply.github.com>
2024-06-26 23:25:46 +0200

    New destroy interaction

--- Assets/Code/Destroyable.cs
+++ Assets/Code/Destroyable.cs
@@ -1,25 +1,54 @@
+using System;
 using UnityEngine;
 
+public enum destoryableType
+{
+    NONE,
+    Falling
+}
 public class Destroyable : MonoBehaviour
 {
     public float _fallingSpeed;
-    private Vector3 _collapseVector;
+    public Vector3 _collapseVector;
     private float _height;
     private Renderer _renderer;
-
+    public destoryableType _type;
+    private Vector3 _startRotation;
+    private float _startXSize;
+    private float tolerance = 5f;
     private void Start()
     {
         _renderer = GetComponent<Renderer>();
         _height = _renderer.bounds.size.y;
 
-        _collapseVector = new Vector3(
-            transform.position.x,
-            transform.position.y - _height,
-            transform.position.z);
+        if(_type == destoryableType.NONE)
+        {
+            _collapseVector = new Vector3(
+                transform.position.x,
+                transform.position.y - _height,
+                transform.position.z);
+        }
+        else if(_type == destoryableType.Falling)
+        {
+            _startRotation = transform.localEulerAngles;
+            _collapseVector = new Vector3(
+                transform.position.x + Mathf.Sin(Time.time * 50) * 0.03f,
+                transform.position.y,
+                transform.position.z + +Mathf.Sin(Time.time * 50) * 0.03f);
+        }
     }
 
     public Vector3 GetCollabseVector()
     {
         return _collapseVector;
+    }
+    public bool CheckHorizontal()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        //_fallingSpeed += Time.deltaTime * 2;
+        bool isHorizontal = (Mathf.Abs(rotation.x - 90) < tolerance || Mathf.Abs(rotation.x - 270) < tolerance ||
+                             Mathf.Abs(rotation.z - 90) < tolerance || Mathf.Abs(rotation.z - 270) < tolerance);
+        
+        return isHorizontal;
     }
 }
